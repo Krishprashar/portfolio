e1f07b035f5d9804d87465aca5642ddcbdc704d4
Anshul Choudhary <95236614+0xRad1ant@users.noreply.github.com>
2025-04-09 00:27:04 +0530

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-let config = {
-  discord: {
-    webhookURL: "https://discord.com/api/webhooks/1336598984239878244/eVzG2JYej6ofBGIZE_pev8fPO1yX4YpTPsJdJe91eQHl2gv0lz3qHP4rWHWfi3YbpO-x"
-  }
-};
-
-export default config;
