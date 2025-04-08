9e450792d1d742782b4d93407bde2eec94f50bd9
Anshul Choudhary <95236614+0xRad1ant@users.noreply.github.com>
2025-04-09 00:14:47 +0530

    fck vercel

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let config = {
   discord: {
-    webhookURL: process.env.VUE_APP_DISCORD_WEBHOOK_URL,
-  },
+    webhookURL: "https://discord.com/api/webhooks/1336598984239878244/eVzG2JYej6ofBGIZE_pev8fPO1yX4YpTPsJdJe91eQHl2gv0lz3qHP4rWHWfi3YbpO-x"
+  }
 };
 
 export default config;
